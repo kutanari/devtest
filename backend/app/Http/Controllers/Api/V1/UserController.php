3a1398999a03ba630d4dfae5950584eedfacc91f
Fery Tarigan <kutanaridesain@gmail.com>
2023-11-24 18:11:13 +0700

    implement create user API functionality

--- backend/app/Http/Controllers/Api/V1/UserController.php
+++ backend/app/Http/Controllers/Api/V1/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
 use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
@@ -12,5 +14,14 @@ class UserController extends Controller
     {
         $users = User::all();
         return new UserCollection($users);
+    }
+
+    public function store(UserStoreRequest $request): UserResource
+    {
+
+        $validated = $request->validated();
+        $user = User::create($validated);
+
+        return new UserResource($user);
     }
 }
