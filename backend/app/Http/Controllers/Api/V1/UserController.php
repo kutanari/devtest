dc5c138b7c558343591a85b03ef13007589245d0
Fery Tarigan <kutanaridesain@gmail.com>
2023-11-24 17:49:51 +0700

    implement list user API

--- backend/app/Http/Controllers/Api/V1/UserController.php
+++ backend/app/Http/Controllers/Api/V1/UserController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\UserCollection;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    
+    public function index(): UserCollection
+    {
+        $users = User::all();
+        return new UserCollection($users);
+    }
 }
