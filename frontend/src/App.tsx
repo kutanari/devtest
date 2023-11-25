221606b33316a2a2565ba48776337a7e930630a1
Fery Tarigan <kutanaridesain@gmail.com>
2023-11-25 11:23:41 +0700

    list users page implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router";
+import Home from "./components/Home";
+import UserIndex from "./components/users/UserIndex";
 import Layout from "./components/Layout";
 
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
             <div className="bg-slate-200">
                 <Routes>
                     <Route path="/" element={<Layout />} >
+                        <Route index element={<Home />} />
+                        <Route path="/users" element={<UserIndex />} />
                     </Route>
                 </Routes>
             </div>
