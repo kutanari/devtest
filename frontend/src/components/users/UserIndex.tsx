cd3dce2460e74847cb378502b304e3f0a19df064
Fery Tarigan <kutanaridesain@gmail.com>
2023-11-25 14:38:38 +0700

    added page container, and load the base  API url configuration from .env file

--- frontend/src/components/users/UserIndex.tsx
+++ frontend/src/components/users/UserIndex.tsx
@@ -1,5 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import PageContainer from "../PageContainer";
 
 interface User {
     id: number;
@@ -15,7 +17,10 @@ const UserIndex: React.FC = () => {
         const getUsers = async () => {
             try {
                 const response: AxiosResponse = await axios.get(
-                    env.VITE_API_BASE_URL + "/api/" + env.VITE_API_VERSION + "/users"
+                    env.VITE_API_BASE_URL +
+                        "/api/" +
+                        env.VITE_API_VERSION +
+                        "/users"
                 );
                 const userData: User[] = response.data.data;
                 setUsers(userData);
@@ -28,68 +33,70 @@ const UserIndex: React.FC = () => {
     }, []);
 
     return (
-        <>
-            <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
-                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
-                    <div className="rounded-t mb-0 px-4 py-3 border-0">
-                        <div className="flex flex-wrap items-center">
-                            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
-                                <h3 className="font-semibold text-base text-blueGray-700">
-                                    Users List
-                                </h3>
-                            </div>
-                            <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-                                <button
-                                    className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                    type="button"
-                                >
-                                    New +
-                                </button>
-                            </div>
-                        </div>
+        <PageContainer>
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+                <div className="flex flex-wrap items-center">
+                    <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                        <h3 className="font-semibold text-base text-blueGray-700">
+                            Users List
+                        </h3>
                     </div>
-
-                    <div className="block w-full overflow-x-auto">
-                        <table className="items-center bg-transparent w-full border-collapse ">
-                            <thead>
-                                <tr>
-                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Name
-                                    </th>
-                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Email
-                                    </th>
-                                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Option
-                                    </th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                {users.map((user) => (
-                                    <tr key={user.id}>
-                                        <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
-                                            {user.name}
-                                        </th>
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                                            {user.email}
-                                        </td>
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                        <button className="bg-green-500 text-white active:bg-green-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                                            Edit
-                                        </button>
-                                        <button className="bg-red-500 text-white active:bg-red-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                                            Delete
-                                        </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                    <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                        <Link
+                            to="/user/create"
+                            className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        >
+                            New +
+                        </Link>
                     </div>
                 </div>
             </div>
-        </>
+
+            <div className="block w-full overflow-x-auto">
+                <table className="items-center bg-transparent w-full border-collapse ">
+                    <thead>
+                        <tr>
+                            <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Name
+                            </th>
+                            <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Email
+                            </th>
+                            <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                Option
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user.id}>
+                                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                                    {user.name}
+                                </th>
+                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                    {user.email}
+                                </td>
+                                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                    <button
+                                        className="bg-green-500 text-white active:bg-green-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                    >
+                                        Edit
+                                    </button>
+                                    <button
+                                        className="bg-red-500 text-white active:bg-red-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </PageContainer>
     );
 };
 
