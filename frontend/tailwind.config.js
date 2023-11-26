b82c30b34e654ecf4a459541972c5c56d9e03a26
Fery Tarigan <kutanaridesain@gmail.com>
2023-11-26 18:00:49 +0700

    custom aria for tailwind, so it can support custom class to show error in the form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      aria: {
+        error: 'error="true"',
+      }
+    },
   },
   plugins: [],
 }
