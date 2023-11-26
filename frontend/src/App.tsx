7ec79955dbe502cdc80db289785f372ab7a48852
Fery Tarigan <kutanaridesain@gmail.com>
2023-11-26 18:00:01 +0700

    implement create user feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Home from "./components/Home";
 import UserIndex from "./components/users/UserIndex";
 import Layout from "./components/Layout";
+import UserCreate from "./components/users/UserCreate";
 import { UserProvider } from "./Context/UserContext";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
                     <Route path="/" element={<Layout />} >
                         <Route index element={<Home />} />
                         <Route path="/users" element={<UserIndex />} />
+                        <Route path="/user/create" element={<UserCreate />} />
                     </Route>
                 </Routes>
             </UserProvider>
--- frontend/src/Context/UserContext.tsx
+++ frontend/src/Context/UserContext.tsx
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import React, { ReactNode, createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 type User = {
     id: number;
@@ -9,9 +10,11 @@ type User = {
 
 type UserContextProps = {
     users: User[];
-    getUsers: (Promise<void>);
-    // user: User | null;
-    // setUser: (user: User | null) => void;
+    storeUser: Promise<void>;
+    errors: object;
+    formData: object;
+    getUsers: (Promise<User[] | null>);
+    setFormData: object;
 }
 
 
@@ -22,21 +25,50 @@ const UserContext = createContext<UserContextProps | undefined>(undefined);
 
 export const UserProvider: React.FC<{children: ReactNode}> = ({ children }) => {
     const [users, setUsers] = React.useState<User[]>([]);
+    const [formData, setFormData] = React.useState<object>({
+        name: "",
+        email: ""
+    });
+    const [errors, setErrors] = React.useState({});
+    const navigate = useNavigate();
 
-    
-    const getUsers = async () => {
+    const getUsers = async (): Promise<User[] | null> => {
         try {
-            const response: AxiosResponse = await axios.get( "/users" );
+            const response: AxiosResponse = await axios.get<User[]>( "/users" );
             const userData: User[] = response.data.data;
             setUsers(userData);
+            return userData;
         } catch (error) {
             console.error("Error fetching user data", error);
+            return null;
         }
     };
 
-    return <UserContext.Provider value={{users, getUsers}}>{ children }</UserContext.Provider>
+    const onChange = (e: Event) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const storeUser = async (e: Event) => {
+        e.preventDefault();
+        try {
+            await axios.post<User>("/users", formData);
+            setFormData({
+                name: "",
+                email: ""
+            });
+            setErrors({});
+            navigate("/users");
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                setErrors(error?.response?.data.errors);
+            } else {
+                console.error('Error', error)
+            }
+        }
+    }
+
+    return <UserContext.Provider value={{users, getUsers, storeUser, formData, setFormData, onChange, errors}}>{ children }</UserContext.Provider>
 }
-
-
 
 export default UserContext;
